20d57fb2a1df4ada267df02aaf96f7f59e9011a8
jis22u <jis22u@gmail.com>
2023-05-01 17:53:00 +0900

    Feat 마커타입 선택 버튼

--- frontend/vue3pwa/src/components/map/MarkerTypeButton.vue
+++ frontend/vue3pwa/src/components/map/MarkerTypeButton.vue
@@ -1,1 +1,29 @@
-﻿
+﻿<template>
+  <v-col
+    cols="6"
+    sm="12"
+    class="py-2"
+  >
+    <v-btn-toggle
+      v-model="toggle_multiple"
+      background-color="primary"
+      dark
+      multiple
+    >
+      <v-btn>
+        버드스트라이크
+      </v-btn>
+
+      <v-btn>
+        새발견
+      </v-btn>
+    </v-btn-toggle>
+  </v-col>
+</template>
+<script>
+  export default {
+    data: () => ({
+      toggle_multiple: [0,1],
+    }),
+  }
+</script>
--- frontend/vue3pwa/src/views/MapView.vue
+++ frontend/vue3pwa/src/views/MapView.vue
@@ -1,11 +1,14 @@
 ﻿<template>
   <div>
+    <MarkerTypeButton></MarkerTypeButton>
+    
     <KakaoMap></KakaoMap>
   </div>
 </template>
 
 <script setup>
 import KakaoMap from "@/components/map/KakaoMap.vue"
+import MarkerTypeButton from "@/components/map/MarkerTypeButton.vue";
 </script>
 
 <style lang="scss" scoped>
