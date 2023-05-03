6296378b4efab5b5a62d800e080d3f405d172abe
조은서 <cho50321@naver.com>
2023-05-03 17:33:48 +0900

    Chore: CORS 및 Swagger 설정
    CorsConfig 파일 추가 및 Swagger 종속성 추가

--- backend/birdchain/src/main/java/com/ssafy/birdchain/api/controller/CommentController.java
+++ backend/birdchain/src/main/java/com/ssafy/birdchain/api/controller/CommentController.java
@@ -5,8 +5,8 @@ import com.ssafy.birdchain.common.db.dto.request.CommentAddReqDTO;
 import com.ssafy.birdchain.common.db.dto.request.CommentDeleteReqDTO;
 import com.ssafy.birdchain.common.db.dto.request.CommentModifyReqDTO;
 import com.ssafy.birdchain.common.db.dto.util.ResponseDTO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -17,7 +17,7 @@ import java.io.IOException;
 
 @Log4j2
 @RestController
-@Api(tags = {"댓글 API"})
+@Tag(name = "댓글 API")
 @RequiredArgsConstructor
 @RequestMapping(value = "/map/comments")
 public class CommentController {
@@ -25,27 +25,27 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping
-    @ApiOperation(value = "댓글 조회")
+    @Operation(summary = "댓글 조회")
     public ResponseEntity<ResponseDTO> commentAllList(@RequestParam Long id) {
         return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, "조회에 성공했습니다.", commentService.findAllComment(id)));
     }
 
     @PostMapping
-    @ApiOperation(value = "댓글 등록")
+    @Operation(summary = "댓글 등록")
     public ResponseEntity<ResponseDTO> commentAdd(@RequestBody CommentAddReqDTO commentAddReqDTO) {
         commentService.addComment(commentAddReqDTO);
         return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, "등록에 성공했습니다."));
     }
 
     @PutMapping
-    @ApiOperation(value = "댓글 수정")
+    @Operation(summary = "댓글 수정")
     public ResponseEntity<ResponseDTO> commentModify(@RequestBody CommentModifyReqDTO commentModifyReqDTO) {
         commentService.modifyComment(commentModifyReqDTO);
         return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, "수정에 성공했습니다."));
     }
 
     @PutMapping("/delete")
-    @ApiOperation(value = "댓글 삭제")
+    @Operation(summary = "댓글 삭제")
     public ResponseEntity<ResponseDTO> commentDelete(@RequestBody CommentDeleteReqDTO commentDeleteReqDTO) throws IOException {
         commentService.deleteMarker(commentDeleteReqDTO);
         return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, "삭제에 성공했습니다."));
