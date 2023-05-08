3f22432338c7e5b6cdc3ef82992f38c8a8b5e341
mayril <inho806901@gmail.com>
2023-05-08 17:20:59 +0900

    update card

--- frontend/vue3pwa/src/components/map/CommentCardRegist.vue
+++ frontend/vue3pwa/src/components/map/CommentCardRegist.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="form-container">
-    <form @submit.prevent>
+
+    <!-- <form >
       <div class="form-group">
-        <label>아 이 디</label>
         <input type="text" v-model="form.nickname" />
       </div>
       <div class="form-group">
-        <label>비밀번호</label>
         <input type="password" v-model="form.password" />
       </div>
       <div class="form-group">
-        <label for="content">내 용</label>
         <textarea id="content" v-model="form.content"></textarea>
       </div>
-    </form>
-    <button type="submit">등록</button>
+    </form> -->
+      <form @submit.prevent class="form-input">
+        <ul class="wrapper">
+          <li class="form-row">
+            <label for="name">아이디</label>
+            <input type="text" v-model="form.nickname" />
+          </li>
+          <li class="form-row">
+            <label for="townborn">비밀번호</label>
+            <input type="password" v-model="form.password" />
+          </li>
+          <li class="form-row">
+            <label for="email">내용</label>
+            <textarea id="content" v-model="form.content"></textarea>
+          </li>
+        </ul>
+        <button type="submit">등록</button>
+
+      </form>
   </div>
 </template>
 <script setup>
@@ -27,35 +42,42 @@ const form = ref({
 });
 </script>
 <style scoped>
-.form-container {
+
+.form-input{
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+  flex-direction: row;
+  margin-top: 250px;
 }
-
-.form-group {
+.wrapper {
+  list-style-type: none;
+  padding: 0;
+  border-radius: 3px;
+}
+.form-row {
   display: flex;
-  margin-bottom: 10px;
+  justify-content: flex-end;
+  padding: .5em;
+}
+.form-row > label {
+  padding: .5em 1em .5em 0;
+  flex: 1;
+}
+.form-row > input {
+  background: rgb(230, 230, 230);
+  flex: 3;
 }
 
-label {
-  margin-bottom: 5px;
-  padding-right: 5px;
-}
-
-input[type="text"],
-input[type="password"],
-textarea {
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid gray;
-  border-radius: 4px;
-  outline: none;
+.form-row > textarea {
+  background: rgb(230, 230, 230);
+  flex: 3;
 }
 
 button[type="submit"] {
-  padding: 10px 20px;
+  width: 50px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 20px;
+  /* padding: 10px 20px; */
   background-color: #4caf50;
   color: white;
   font-size: 16px;
