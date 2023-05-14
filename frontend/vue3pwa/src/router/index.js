2862f77662a9d15c5d9b29766a3598fc202c4100
unknown <smhyun128@naver.com>
2023-05-14 21:09:04 +0900

    Feat: 기부할때 페이지 이동 막기

--- frontend/vue3pwa/src/router/index.js
+++ frontend/vue3pwa/src/router/index.js
@@ -8,9 +8,27 @@ import GameView from "@/views/GameView.vue";
 import GameOverView from "@/views/GameOverView.vue";
 import NftView from "@/views/NftView.vue";
 import MapView from "@/views/MapView.vue";
-import { useAccountStore } from "@/stores/accountStore";
 import TestView from "@/views/TestView.vue";
+import { createWeb3Instance } from "@/web3";
+import { getCheckoutAccount } from "@/api/checkAccount";
+import { useAccountStore } from "@/stores/accountStore";
 
+const checkAccount = async () => {
+  const web3 = await createWeb3Instance();
+  if (web3) {
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    const res = await getCheckoutAccount(account);
+    console.log(res);
+    console.log(res.data);
+    if (res.data.value === null) {
+      console.log("아직 기부 안함");
+      return false; // 아직 기부하지 않은 경우
+    } else {
+      return true; // 기부한 경우
+    }
+  }
+};
 const routes = [
   {
     path: "/",
@@ -31,12 +49,16 @@ const routes = [
     path: "/donate",
     name: "donateView",
     component: DonateView,
-    beforeEnter: (to, from, next) => {
-      const storedImages = localStorage.getItem("images");
-      if (storedImages) {
-        next({ name: "nftView" });
+    beforeEnter: async (to, from, next) => {
+      const hasDonated = await checkAccount();
+      if (hasDonated) {
+        console.log("가나");
+        const accountStore = useAccountStore();
+        accountStore.donate();
+        next("/nft"); // 기부한 경우 NFT 페이지로 이동
       } else {
-        next();
+        console.log("다라");
+        next(); // 기부하지 않은 경우 원래 경로로 이동
       }
     },
   },
@@ -54,15 +76,13 @@ const routes = [
     path: "/nft",
     name: "nftView",
     component: NftView,
-    beforeEnter: (to, from, next) => {
-      // ...
-      const images = localStorage.getItem("myNFTImages");
-      const accountStore = useAccountStore();
-
-      if (images || accountStore.hasDonated) {
+    beforeEnter: async (to, from, next) => {
+      const hasDonated = useAccountStore().hasDonated;
+      console.log(hasDonated);
+      if (hasDonated) {
         next();
       } else {
-        next({ name: "donateView" });
+        next("/donate");
       }
     },
   },
