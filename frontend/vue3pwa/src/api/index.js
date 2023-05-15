2789f0f9ba30ffc8802d3dfeaafbf74bc03b1166
현승민 <smhyun128@naver.com>
2023-05-15 09:59:16 +0900

    Feat: checkAccount 추가

--- frontend/vue3pwa/src/api/index.js
+++ frontend/vue3pwa/src/api/index.js
@@ -12,4 +12,4 @@ function create(AXIOS_CALL_URL) {
 
 export const markers = create(BASE_URL+'/map/markers');
 export const comments = create(BASE_URL+'/map/comments');
-
+export const checkAccount = create(BASE_URL + "/check");
