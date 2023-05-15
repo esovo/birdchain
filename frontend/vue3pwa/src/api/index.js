c0e81f1976fd2bfa806af0793fb12d674e4fec1d
unknown <smhyun128@naver.com>
2023-05-15 09:57:05 +0900

    Feat: index.js에 checkAccount 빠짐

--- frontend/vue3pwa/src/api/index.js
+++ frontend/vue3pwa/src/api/index.js
@@ -4,12 +4,12 @@ const BASE_URL = "/api";
 
 function create(AXIOS_CALL_URL) {
   const instance = axios.create({
-      baseURL: AXIOS_CALL_URL,
-      withCredentials: true,
-    });
+    baseURL: AXIOS_CALL_URL,
+    withCredentials: true,
+  });
   return instance;
 }
 
-export const markers = create(BASE_URL+'/map/markers');
-export const comments = create(BASE_URL+'/map/comments');
-
+export const markers = create(BASE_URL + "/map/markers");
+export const comments = create(BASE_URL + "/map/comments");
+export const checkAccount = create(BASE_URL + "/check");
