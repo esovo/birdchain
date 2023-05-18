e85c65e57c5da42c87d2a729638d32fc034daaec
unknown <smhyun128@naver.com>
2023-05-18 14:40:52 +0900

    Fix: Suspense 컴포넌트를 사용해 비동기 처리

--- frontend/vue3pwa/src/views/MypageView.vue
+++ frontend/vue3pwa/src/views/MypageView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="mypage">
-    <mynft-componenet></mynft-componenet>
-
-    <minting-component></minting-component>
+    <Suspense>
+      <template #default>
+        <mynft-componenet v-if="web3Loaded"></mynft-componenet>
+        <minting-component v-if="web3Loaded"></minting-component>
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
   </div>
 </template>
 <script>
+import { ref } from "vue";
 import MintingComponent from "@/components/mypage/MintingComponent.vue";
 import MynftComponenet from "@/components/mypage/MynftComponene.vue";
 import { checkWeb3Instance } from "@/web3.js";
@@ -15,9 +22,12 @@ export default {
     MynftComponenet,
     MintingComponent,
   },
-
-  beforeCreate() {
-    checkWeb3Instance();
+  setup() {
+    const web3Loaded = ref(false);
+    checkWeb3Instance().then(() => {
+      web3Loaded.value = true;
+    });
+    return { web3Loaded };
   },
 };
 </script>
